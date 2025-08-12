cb56cccc4b31ea110913bc1616b7fafdbced39f2
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-08-12 19:50:33 +0300

    Add project files.

---
+++ MvcCV/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+
+namespace MvcCV.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var values = db.TBLAbout.ToList();
+            return View(values);
+        }
+    }
+}
