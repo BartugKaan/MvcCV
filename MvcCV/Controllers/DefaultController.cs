833f5f3b8d1b943e12e4f1db628eaa9dd2e85911
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-08-13 14:32:18 +0300

    Add new sections for Experience, Education, Skills, etc.
    Implemented new action methods in DefaultController for
    Experience, Education, Skills, Hobbies, and Certificates.
    Created corresponding view files to display dynamic content
    for each section. Updated Index.cshtml to render these
    sections using partial views, enhancing modularity and
    maintainability of the application.

--- MvcCV/Controllers/DefaultController.cs
+++ MvcCV/Controllers/DefaultController.cs
@@ -12,5 +12,35 @@ namespace MvcCV.Controllers
             var values = db.TBLAbout.ToList();
             return View(values);
         }
+
+        public PartialViewResult Experience()
+        {
+            var values = db.TBLExperience.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Education()
+        {
+            var values = db.TBLEducation.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Skills()
+        {
+            var values = db.TBLSkill.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Hobbies()
+        {
+            var values = db.TBLHobby.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult Certificates()
+        {
+            var values = db.TBLCertificates.ToList();
+            return PartialView(values);
+        }
     }
 }
